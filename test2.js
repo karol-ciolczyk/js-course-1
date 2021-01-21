43770bf4062bde0669245ba9d27adeba72839aa7
Karol Ciołczyk <dyweko@gmail.com>
2021-01-21 22:11:07 +0100

    form validation

--- test2.js
+++ test2.js
@@ -1,18 +1,13 @@
-myForm.name.value
-"nkarol"
-myForm.colour.value
-"Red"
-myForm.colour.value
-"Blue"
-myForm.name.onfocus = function(){
-    myForm.name.style.border = "4px solid pink";
-}
-ƒ (){
-    myForm.name.style.border = "4px solid pink";
-}
-myForm.name.onblur = function(){
-    myForm.name.style.border = "none";
-}
-ƒ (){
-    myForm.name.style.border = "none";
-}
+var myForm = document.forms.myForm;
+var message = document.getElementById("message");
+
+myForm.onsubmit = function(){
+
+        if(myForm.name.value == ""){
+            message.innerHTML = "please enter name";
+        } else{
+            message.innerHTML = "";
+            return true;
+        }
+
+};
