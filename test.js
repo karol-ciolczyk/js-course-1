10d73312175249b840f04f9c321b8c0de4ab2afb
Karol Ciołczyk <dyweko@gmail.com>
2021-01-21 10:06:23 +0100

    using loops

--- test.js
+++ test.js
@@ -1,9 +1,6 @@
 var links = document.getElementsByTagName("a");
 
-for(i = 1; i <= links.length; i++){
+for( i = 0; i < links.length; i++){
 
-    console.log("this is link number" + i);
-
+    links[i].className = "link-" + i;
 }
-
-document.write("all links now looped");
