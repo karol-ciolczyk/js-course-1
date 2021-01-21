cdffc501e3df42ee1a425953694d2c761bd0ef03
Karol Ciołczyk <dyweko@gmail.com>
2021-01-21 20:56:24 +0100

    theoneclick event

---
+++ test2.js
@@ -0,0 +1,16 @@
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclinck = function(){
+
+    if(content.className == "open"){
+        
+        content.className = "";
+        button.innerHTML = "Show More";
+     }  else{
+
+        content.className = "open";
+        button.innerHTML = "Show Less";
+     }
+
+};
