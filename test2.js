7485ffbd316960ec2a5df34eb93c4792c4e742ca
Karol Ciołczyk <dyweko@gmail.com>
2021-01-21 21:11:06 +0100

    window onload event

--- test2.js
+++ test2.js
@@ -1,16 +1,27 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function setUpEvents(){
 
-button.onclinck = function(){
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    
+    button.onclinck = function(){
+    
+        if(content.className == "open"){
+            
+            content.className = "";
+            button.innerHTML = "Show More";
+         }  else{
+    
+            content.className = "open";
+            button.innerHTML = "Show Less";
+         }
+    
+    };
+    
+}
 
-    if(content.className == "open"){
-        
-        content.className = "";
-        button.innerHTML = "Show More";
-     }  else{
 
-        content.className = "open";
-        button.innerHTML = "Show Less";
-     }
+window.onload = function(){
+
+    setUpEvents();
 
 };
