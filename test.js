1c29b90bd10bb1419b423b37f8c935a10c979dc7
Karol Ciołczyk <dyweko@gmail.com>
2021-01-21 17:46:29 +0100

    Adding elements to the Dom

--- test.js
+++ test.js
@@ -1,18 +1,22 @@
-var title = document.getElementById("title-footer");
+var newLi = document.createElement("li");
 undefined
-title.style.top = "45";
-"45"
-title.setAttribute("style", "position: relative; left: 30px; top: 20px; background-color: blue;");
+newLi
+<li>​</li>​
+var newA = document.createElement("a");
 undefined
-title.style.background
-""
-title.style.backgroundColor = "red";
-"red"
-title.style.top = "10";
-"10"
-title.style.top = "40";
-"40"
-title.style.positionTop = "40";
-"40"
-title.style.left = "40px"
-"40px"
+newA
+<a>​</a>​
+var menu = document.getElementById("main-navigation").getElementsByTagName("ul")[0];
+undefined
+menu
+<ul>​…​</ul>​<li>​…​</li>​<li>​…​</li>​<li>​…​</li>​<li>​…​</li>​<li>​…​</li>​<li>​…​</li>​</ul>​
+menu.appendChild(newLi);
+<li>​</li>​
+newLi.appendChild(newA);
+<a>​</a>​
+newA.innerHTML = "News";
+"News"
+menu.insertBefore(newLi, menu.getElementsByTagName("li")[0];
+VM8172:1 Uncaught SyntaxError: missing ) after argument list
+menu.insertBefore(newLi, menu.getElementsByTagName("li")[0]);
+<li>​…​</li>​<a>​News​</a>​</li>​
