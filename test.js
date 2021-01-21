518817c1566c6b6b0b05e8527b738cbef74d8cb5
Karol Ciołczyk <dyweko@gmail.com>
2021-01-21 17:24:16 +0100

    changing CSS style

--- test.js
+++ test.js
@@ -1,24 +1,18 @@
-var Car = function(maxSpeed, driver){
-
-    this.maxSpeed = maxSpeed;
-    this.driver = driver
-    this.drive = function(speed, time){
-        console.log(speed * time);
-
-    };
-    this.logDriver = function(){
-        console.log("driver name is - " + this.driver);
-    };
-
-}
-
-var myCar = new Car(60, "Karol ride");
-var myCar1 = new Car(60, "Karol ride");
-var myCar2 = new Car(60, "Karol ride");
-var myCar3 = new Car(60, "Karol rrrrrride");
-var myCar4 = new Car(60, "Karol ride");
-var myCar5 = new Car(60, "Karol rirrrrrrrde");
-var myCar6 = new Car(60, "Karol ride");
-
-myCar.drive(23,5);
-myCar5.logDriver()
+var title = document.getElementById("title-footer");
+undefined
+title.style.top = "45";
+"45"
+title.setAttribute("style", "position: relative; left: 30px; top: 20px; background-color: blue;");
+undefined
+title.style.background
+""
+title.style.backgroundColor = "red";
+"red"
+title.style.top = "10";
+"10"
+title.style.top = "40";
+"40"
+title.style.positionTop = "40";
+"40"
+title.style.left = "40px"
+"40px"
